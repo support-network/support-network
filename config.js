74b401c7d4e460291a5038d672d1198d33ede759
123 <ludka388@gmail.com>
2026-04-05 21:23:05 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRLd-2PXRjkL6f59dEplWadvsRuUBHDmY",
+    authDomain: "support-network-33699.firebaseapp.com",
+    projectId: "support-network-33699",
+    storageBucket: "support-network-33699.firebasestorage.app",
+    messagingSenderId: "541408271881",
+    appId: "1:541408271881:web:588b8eb4e93b102a0de2d8"
+};
+
+// Инициализация
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const { useState, useEffect, useRef } = React;
