6a80eef38bc94fa3534bab56fd65b85b22a9b693
123 <ludka388@gmail.com>
2026-04-05 22:48:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,19 @@
+// Конфигурация Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCRLd-2PXRjkL6f59dEplWadvsRuUBHDmY",
-    authDomain: "support-network-33699.firebaseapp.com",
-    projectId: "support-network-33699",
-    storageBucket: "support-network-33699.firebasestorage.app",
-    messagingSenderId: "541408271881",
-    appId: "1:541408271881:web:588b8eb4e93b102a0de2d8"
+  apiKey: "AIzaSyCrLD-2PXRjkL6f59deplWadvsRuUBHDmY",
+  authDomain: "support-network-33699.firebaseapp.com",
+  projectId: "support-network-33699",
+  storageBucket: "support-network-33699.firebasestorage.app",
+  messagingSenderId: "541408271881",
+  appId: "1:541408271881:web:588b8eb4e93b102a0de2d8"
 };
 
-if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-const { useState, useEffect } = React;
+// Инициализация
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Делаем переменные глобальными (window), чтобы другие файлы их видели
+window.auth = firebase.auth();
+window.db = firebase.firestore();
+window.appId = 'support-net-v2';
