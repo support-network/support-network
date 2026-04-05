4cec14eea9f61bc07f794fcc32a2f944ab767fff
123 <ludka388@gmail.com>
2026-04-05 22:51:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Конфигурация Firebase
+// Данные для подключения
 const firebaseConfig = {
   apiKey: "AIzaSyCrLD-2PXRjkL6f59deplWadvsRuUBHDmY",
   authDomain: "support-network-33699.firebaseapp.com",
@@ -8,12 +8,12 @@ const firebaseConfig = {
   appId: "1:541408271881:web:588b8eb4e93b102a0de2d8"
 };
 
-// Инициализация
+// Инициализация (используем старый синтаксис compat)
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-// Делаем переменные глобальными (window), чтобы другие файлы их видели
+// Делаем переменные глобальными, чтобы app.js их увидел
 window.auth = firebase.auth();
 window.db = firebase.firestore();
 window.appId = 'support-net-v2';
