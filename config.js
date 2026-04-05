f02b7ab54e9b30d1332ef9383a714a7ccd7ad3a0
123 <ludka388@gmail.com>
2026-04-05 22:54:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// Данные для подключения
 const firebaseConfig = {
   apiKey: "AIzaSyCrLD-2PXRjkL6f59deplWadvsRuUBHDmY",
   authDomain: "support-network-33699.firebaseapp.com",
@@ -8,12 +7,10 @@ const firebaseConfig = {
   appId: "1:541408271881:web:588b8eb4e93b102a0de2d8"
 };
 
-// Инициализация (используем старый синтаксис compat)
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-// Делаем переменные глобальными, чтобы app.js их увидел
 window.auth = firebase.auth();
 window.db = firebase.firestore();
 window.appId = 'support-net-v2';
